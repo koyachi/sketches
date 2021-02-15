64b49e1d8642264f2f3da21a61bf59d345c01b2b
koyachi <rtk2106@gmail.com>
2021-02-15 20:18:19 +0900

    gallery ui

--- 2021-02-11-pythonista-image/access_photos.py
+++ 2021-02-11-pythonista-image/access_photos.py
@@ -1,5 +1,7 @@
 from PIL import Image
 import photos
+import ui
+import io
 
 # too slow ;-(
 def get_all_assets():
@@ -20,10 +22,26 @@ def get_test_album_assets():
 			break
 	return album.assets
 
+# via https://qiita.com/kermit71/items/8212028db5fccc9f27fe
+def conver_image_pil2ui(image_in):
+	with io.BytesIO() as bio:
+		image_in.save(bio, 'JPEG')
+		image_out = ui.Image.from_data(bio.getvalue())
+	del bio
+	return image_out
+
 def main():
-	assets = get_test_album_photos()
-	for asset in assets:
-		asset.get_image().show()
+	view = ui.load_view('gallery')
+	view.present('sheet')
+	assets = get_test_album_assets()
+
+	#for asset in assets:
+	#	asset.get_image().show()
+
+	for i in range(len(assets)):
+		image_id = f'imageview{i}'
+		# view[image_id].image = assets[i].get_ui_image()
+		view[image_id].image = conver_image_pil2ui(assets[i].get_image())
 
 if __name__ == '__main__':
 	main()
